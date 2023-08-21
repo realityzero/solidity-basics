9db33444eb6c378d93d8e43855e9ac74f0471dfe
Neshantt <33288462+realityzero@users.noreply.github.com>
2023-08-21 23:32:46 +0530

    storage variable

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -4,4 +4,8 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+  }
 }
