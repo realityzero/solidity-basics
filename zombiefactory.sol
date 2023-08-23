7ef4a62503e277528ae838e5b6d74ce1a2eed00c
Neshantt <33288462+realityzero@users.noreply.github.com>
2023-08-23 23:43:07 +0530

    ownable contract

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,6 +1,8 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-contract ZombieFactory {
+import "./ownable.sol"
+
+contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
@@ -32,6 +34,7 @@ contract ZombieFactory {
     function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
